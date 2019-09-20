34598bc8c02ddafcc27f3948b3b0d32c8dc85330
PS1717 <38763217+Priyabrat1717@users.noreply.github.com>
2019-09-20 08:33:48 +0530

    Update syberia_mirage01a.mk

--- syberia_mirage01a.mk
+++ syberia_mirage01a.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/LYF/mirage01a/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/syberia/common.mk)
+$(call inherit-product, vendor/LYF/mirage01a/mirage01a-vendor.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
